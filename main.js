81c4aaa3503079de9b8ad1761d3d449fdd57156f
junaidjesan2 <junaidjesan2@gmail.com>
2024-01-27 14:28:58 +0600

    tested routing system through if else statement

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const http=require("http")
 
 var server=http.createServer(function(req,res){
-    res.end("Parsire mmh")
+    if(req.url=="/"){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write('<h1>This is Home Page</h1>')
+        res.end()
+    }
+    if(req.url=="/contact"){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write('<h1>This is Contact Page</h1>')
+        res.end()
+    }
 })
 
 server.listen(8000)
-console.log("Server connected");
+console.log("Server");
