0a55cf4121f94fdc580bbfaed5ff3a58dd218c0c
junaidjesan2 <junaidjesan2@gmail.com>
2024-01-28 22:24:28 +0600

    memorized how to change status code

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/about',(req,res)=>{
+res.status(404).end("Page not found...")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
